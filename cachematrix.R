35a5a956344d30fec030b41c1d291aa1660bc17b
Senem Yuzbasioglu <senemyuzbasioglu@gmail.com>
2015-04-14 09:55:44 +0300

    Update cachematrix.R
    Tested with the easy invert test at: https://class.coursera.org/rprog-013/forum/thread?thread_id=127

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a cached matrix using 
+## its helper functions.
+makeCacheMatrix <- function(x = matrix()) 
+{
+    matrixInvert <- NULL
 
-## Write a short comment describing this function
+    ## Sets matrix content
+    setmatrix <- function(y) 
+    {
+        x <<- y
+        matrixInvert <<- NULL
+    }
 
-makeCacheMatrix <- function(x = matrix()) {
+    ## Returns matrix content
+    getmatrix <- function() x
 
+    ## Sets the inverted version of the given matrix
+    ## This is the caching operation.
+    setinvert <- function(invert) matrixInvert <<- invert
+
+    ## Returns the inverted cached matrix
+    getinvert <- function() matrixInvert
+
+    list(setmatrix = setmatrix, 
+         getmatrix = getmatrix,
+         setinvert = setinvert,
+         getinvert = getinvert)
 }
 
+## This function returns the inverted version of a 
+## given matrix.
+##
+## If the inversion of a matrix already exists, it
+## is merely returned. Otherwise, it is calculated
+## and returned.
+cacheSolve <- function(x, ...) 
+{
+    ## Check if the inverted matrix is already cached
+    invert <- x$getinvert()
 
-## Write a short comment describing this function
+    ## Found a cached invert, simply return it.
+    if(!is.null(invert)) 
+    {
+        message("getting cached data")
+        return(invert)
+    }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Cached invert not found
+    ## Get matrix content
+    data <- x$getmatrix()
+
+    ## Compute the invert using the solve function
+    invert <- solve(data, ...)
+
+    ## Cache the invert for later use
+    x$setinvert(invert)
+
+    ## Return the computed invert
+    invert
 }
